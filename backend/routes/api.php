478ecf1541a6dc98caebe29bb766b93c43065e7f
hourensou26 <ktc23a31g0002@edu.kyoto-tech.ac.jp>
2026-06-13 15:59:06 +0900

    logout APIの実装

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -20,4 +20,8 @@ Route::prefix('v1')->group(function () {
     Route::get('restaurants', [StoreController::class, 'index']);
     Route::get('restaurants/{id}', [StoreController::class, 'show']);
     Route::post('auth/login', [AuthController::class, 'login']);
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('auth/logout', [AuthController::class, 'logout']);
+    });
 });
